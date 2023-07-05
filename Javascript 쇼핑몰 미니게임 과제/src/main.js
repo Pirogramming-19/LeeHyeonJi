1de4bd7eaaebaa451185e4f5d42443cc0a110b71
hyeonji lee <hyunji3017@naver.com>
2023-07-05 21:40:22 +0900

    JavaScript - display items

--- Javascript 쇼핑몰 미니게임 과제/src/main.js
+++ Javascript 쇼핑몰 미니게임 과제/src/main.js
@@ -4,10 +4,24 @@ function loadItems() {
     .then(json => json.items);
 }
 
+function displayItems(items) {
+    const container = document.querySelector(".items");
+    container.innerHTML = items.map(item => createHTMLString(item)).join('');
+}
+
+ //배열의 요소 -> HTML li 태그(문자열)로 변경
+function createHTMLString(item) {
+    return `
+        <li class="item">
+            <img src="${item.img}" alt="${item.type}" class="item_thumnail">
+            <span class="item_description">${item.gender}, ${item.size}</span>
+        </li>
+    `;
+}
+
 loadItems()
     .then(items => {
-        console.log(items);
-        // displayItems(items);
+        displayItems(items); //items는 배열
         // setEventListeners(items);
     })
     .catch(console.log);
