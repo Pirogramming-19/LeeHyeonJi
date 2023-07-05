5ded7c1b7103cb12fb20093ed87bbb585f9309a1
hyeonji lee <hyunji3017@naver.com>
2023-07-05 22:07:23 +0900

    JavaScript - handling events

--- Javascript 쇼핑몰 미니게임 과제/src/main.js
+++ Javascript 쇼핑몰 미니게임 과제/src/main.js
@@ -19,9 +19,31 @@ function createHTMLString(item) {
     `;
 }
 
+function onButtonClick(event, items) {
+    const dataset = event.target.dataset;
+    const key = dataset.key;        //ex1) type     ex2) color
+    const value = dataset.value;    //ex1) tshirt   ex2) blue
+
+    //btn 외의 영역을 클릭한 경우
+    if (key==null || value==null) {
+        return;
+    }
+
+    //배열.filter(): 배열에서 특정 데이터만 추출해서 새로운 배열 만들기
+    displayItems(items.filter(item => item[key] === value));
+}
+
+function setEventListeners(items) {
+    const logo = document.querySelector(".logo");
+    logo.addEventListener('click', () => displayItems(items));
+
+    const btns = document.querySelector(".btns"); //이벤트의 위임(이벤트를 한 곳에서만 핸들링하기 위함)
+    btns.addEventListener('click', event => onButtonClick(event, items));
+}
+
 loadItems()
     .then(items => {
         displayItems(items); //items는 배열
-        // setEventListeners(items);
+        setEventListeners(items);
     })
     .catch(console.log);
