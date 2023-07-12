22f50f154d174eb180a9802adf859f54a09e9844
hyeonji lee <hyunji3017@naver.com>
2023-07-12 15:21:28 +0900

    설치하기

---
+++ djangogirls/myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\★GIT_GITHUB\piro-git-practice\djangogirls\myvenv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
