4c4f08c5431fffae32458660f18d8bbcdff066bf
hyeonji lee <hyunji3017@naver.com>
2023-07-14 14:17:46 +0900

    fix: 장고 모델 수정
    모델에 컬럼 추가

--- myMovieReviews/myApp/models.py
+++ myMovieReviews/myApp/models.py
@@ -5,3 +5,7 @@ class Review(models.Model):
     release_year = models.IntegerField()                        # 개봉 년도
     genre = models.CharField(max_length=10)                     # 장르
     rate = models.DecimalField(max_digits=2, decimal_places=1)  # 별점
+    running_time = models.IntegerField()                        # 러닝타임(단위 min)
+    content = models.TextField()                                # 리뷰 내용
+    director = models.CharField(max_length=20)                  # 영화 감독
+    lead = models.CharField(max_length=100)                     # 주연 배우
