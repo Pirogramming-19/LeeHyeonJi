88a3d5eba94cffe24e4400d7e2918e8f775deb78
hyeonji lee <hyunji3017@naver.com>
2023-07-14 12:25:59 +0900

    feat: 장고 모델 생성

--- myMovieReviews/myApp/models.py
+++ myMovieReviews/myApp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Review(models.Model):
+    title = models.CharField(max_length=30)                     # 영화 제목
+    release_year = models.IntegerField()                        # 개봉 년도
+    genre = models.CharField(max_length=10)                     # 장르
+    rate = models.DecimalField(max_digits=2, decimal_places=1)  # 별점
