2101711fcc738e605018e8a66fab3214fad62c7f
hyeonji lee <hyunji3017@naver.com>
2023-07-19 00:48:31 +0900

    장고 모델 생성

--- SWIDEA_SITE/myApp/models.py
+++ SWIDEA_SITE/myApp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Idea(models.Model):
+    # 아이디어명, 이미지, 아이디어 설명, 아이디어 관심도, 예상 개발툴
+    title = models.CharField(max_length=30)
+    image = models.FileField()
+    content = models.TextField()
+    interest = models.IntegerField()
+    devtool = models.CharField(max_length=20)
