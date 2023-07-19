8c21a9b074193556d29a5d6d793d9f96c503dee2
hyeonji lee <hyunji3017@naver.com>
2023-07-20 03:41:55 +0900

    장고 어드민에 모델 추가 등록

--- SWIDEA_SITE/myApp/admin.py
+++ SWIDEA_SITE/myApp/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Idea, Devtool
+from .models import Idea, Devtool, IdeaStar
 
 admin.site.register(Idea)
 admin.site.register(Devtool)
+admin.site.register(IdeaStar)
