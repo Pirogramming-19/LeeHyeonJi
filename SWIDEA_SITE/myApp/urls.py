d80ad25a5c35e02003b20677e8016e74fa8a1382
hyeonji lee <hyunji3017@naver.com>
2023-07-19 11:48:33 +0900

    devtool 관련 url 등록

--- SWIDEA_SITE/myApp/urls.py
+++ SWIDEA_SITE/myApp/urls.py
@@ -3,4 +3,9 @@ from .views import *
 
 urlpatterns = [
     path('', main, name="main"),
+    path('devtool/', search_all_devtool, name="search_all_devtool"),
+    path('devtool/<int:pk>/', search_devtool, name="search_devtool"),
+    path('devtool/create/', create_devtool, name="create_devtool"),
+    path('devtool/<int:pk>/update/', update_devtool, name="update_devtool"),
+    path('devtool/<int:pk>/delete/', delete_devtool, name="delete_devtool"),
 ]
