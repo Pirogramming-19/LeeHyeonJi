7e45ef55c4dee3fb261d323e0f3de5929b0d7363
hyeonji lee <hyunji3017@naver.com>
2023-07-19 11:41:36 +0900

    장고 모델 추가
    Devtool 모델 추가

--- SWIDEA_SITE/myApp/models.py
+++ SWIDEA_SITE/myApp/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 
+class Devtool(models.Model):
+    name = models.CharField(max_length=20)
+    kind = models.CharField(max_length=20)
+    content = models.TextField()
+
 class Idea(models.Model):
     # 아이디어명, 이미지, 아이디어 설명, 아이디어 관심도, 예상 개발툴
     title = models.CharField(max_length=30)
